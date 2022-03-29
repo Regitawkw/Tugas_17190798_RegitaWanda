5cf70cdae7a64bc643870ac73f536177771fc584
Regitawkw <Regitawanda33@gmail.com>
2022-03-29 22:55:11 +0700

    menambahkan fungsi controller,model,view

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h> Perkenalkan </h> <br>";
+        echo " Nama Saya Regita Wanda Kusuma Wardani <br>
+        Saya lahir di Garut 27 Mei 2001 <br>
+        Saya tinggal di kab. Bogor";
+    }
+} 
---
+++ application/views/view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title><b> Latihan 1 </b> </title>
+</head>
+<body>
+    Hallo Kawan... Yuk Kita Belajar Web Programming!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    Ini hasil dari pemodelan dengan methode penjumlahan yaitu 
+    <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
