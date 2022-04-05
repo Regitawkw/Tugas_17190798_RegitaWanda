2651b76a8a9938244f80ab9e23ce205ff2a7b18f
Regitawkw <Regitawanda33@gmail.com>
2022-04-05 15:23:59 +0700

    mengerjakan contoh 5 dan template web sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+
+    <h4> Nama </h4>
+    <ul type="disc">
+        <li> Nama Depan : Regita Wanda </li>
+        <li> Nama Belakang  : Kusuma Wardani </li>
+        <li> Nama Lengkap   : Regita Wanda Kusuma Wardani </li>
+    </ul>
+    <h4> Alamat </h4>
+    <ul type="none">
+        <li> Kp. Cibarengkok, RT 001 RW 003, Pengasinan </li>
+    </ul>
+    <h4> Tempat Lahir </h4>
+    <ul type="none">
+        <li> Garut </li>
+    </ul>
+    <h4> Olahraga Favorite </h4>
+    <ul type="square">
+        <li> Berenang </li>
+        <li> Jogging </li>
+    </ul>
+</section>
